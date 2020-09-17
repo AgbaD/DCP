78e9af77597a7084f4daaeca506694b357f30088
Damilare <blankgodd33@gmail.com>
2020-09-18 00:54:37 +0100

    day 5 - interesting

---
+++ solutions/day5.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# Author: @BlankGodd_
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+
+def grace(a, b):
+    return tuple([a, b])
+
+
+def car(pair):
+    p = pair(grace)
+    return p[0]
+
+
+def cdr(pair):
+    p = pair(grace)
+    return p[-1]
+
+
+if __name__ == "__main__":
+    a, b = 3, 4
+    print(car(cons(a, b)))
+    print(cdr(cons(a, b)))
